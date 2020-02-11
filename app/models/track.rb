1ac8e05c60a7516fb3005b16bf3482417dd670db
Grace Jang <hygracem@gmail.com>
2020-02-11 21:00:56 +0900

    through is fixed

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,10 +1,9 @@
 class Track < ApplicationRecord
-  has_many :artists, through: :artistsTracks
-  has_many :artistsTracks
-  has_many :users, through: :tracksUsers
   has_many :tracksUsers
+  has_many :artistsTracks
+  has_many :artists, through: :artistsTracks
+  has_many :users, through: :tracksUsers
   validates :name, presence: true
-  validates :artists, presence: true
   validates :album, presence: true
   validates :spotify_id, presence: true
 end
