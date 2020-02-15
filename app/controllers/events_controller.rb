a2e2e16fcb0813e9a699d54a2dc8e625b3658da2
Grace Jang <hygracem@gmail.com>
2020-02-15 18:56:06 +0900

    changes wip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,18 @@ class EventsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @user = User.first
+    @events = Event.near([params[:lat], params[:lon]], 10)
+    #event model needs display_name and artist(hash artist in perf array)
   end
 
   def create
   end
 
   def show
+    params[:id]
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :venue)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   has_many :usersEvents
   has_many :users, through: :usersEvents
-  validates :date, presence: true
   validates :venue, presence: true
+  geocoded_by :venue
+  after_validation :geocode
+  # , if: :will_save_change_to_address?
 end
