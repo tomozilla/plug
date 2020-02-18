135c61db77c56c097620bbbfa38b00e595e11a4b
Tomohiro <tmitani@heroku.com>
2020-02-18 20:45:22 +0900

    oauth for spotify

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,20 @@ class User < ApplicationRecord
   has_many :tracks, through: :tracksUsers
   has_many :events, through: :usersEvents
   validates :email, presence: true
-  validates :genres, presence: true
   validates :spotify_id, presence: true
+
+  def self.find_for_spotify(spotify_id:, email:, access_token:, refresh_token:)
+    user = User.find_by(spotify_id: spotify_id)
+    if user
+      user.update(access_token: access_token, refresh_token: refresh_token)
+    else
+      user = User.new(email: email, spotify_id: spotify_id)
+      user.password = Devise.friendly_token[0,20]
+      user.access_token = access_token
+      user.refresh_token = refresh_token
+      user.save
+    end
+
+    return user
+  end
 end
