9f7a86b2eabb362775ffa6921ea35c24c79278bf
Tomohiro <tmitani@heroku.com>
2020-02-22 11:46:18 +0900

    seeds ready for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_20_120515) do
+ActiveRecord::Schema.define(version: 2020_02_22_015740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_02_20_120515) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "artist"
+    t.string "address"
   end
 
   create_table "tracks", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,16 @@ User.destroy_all
 puts "Users Destroyed"
 
 puts "Start Updating databse"
+location_choice = ["Impact Hub", "Lawson", "Maruetsu"]
+address_choice = ["Tokyo, Meguro City, Meguro, 2−11−3", "3 Chome-9-1 Meguro, Meguro City, Tokyo", "2 Chome-21-23 Shimomeguro, Meguro City, Tokyo"]
+
+rand_number = rand(0..2)
+
+new_event = Event.create!(
+  date: Faker::Date.between(from: 2.days.from_now, to: 5.days.from_now),
+  address: address_choice[rand_number],
+  venue: location_choice[rand_number]
+)
 
 # 20.times do
 #   new_artist = Artist.create!(
