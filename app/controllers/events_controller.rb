aa1e93ac6e0270df029dc052294c929b3c6171b2
Tomohiro <tmitani@heroku.com>
2020-02-29 15:37:45 +0900

    adjaxify card when staring

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -30,40 +30,53 @@ class EventsController < ApplicationController
     @event = Event.includes(users: :tracks).find(params[:id])
     @users = @event.users.where.not(id: current_user.id)
     @track_counter = {}
-    @artist_counter = {}
 
     @users.each do |user|
       user.tracks_users.each do |user_track|
         @track_counter[user_track.track_id] ||= [0, user_track.track]
         @track_counter[user_track.track_id][0] += 1
-
-        user_track.track.artists.each do |artist|
-          @artist_counter[artist.id] ||= [0, artist]
-          @artist_counter[artist.id][0] += 1
-        end
       end
-
       @tracks = @track_counter.sort_by { |track_id, data| data.first }.take(20).map { |_, data| data.second }
-      @artists = @artist_counter.sort_by { |artist_id, count| count[0] }.take(20).map { |_, count| count.second }
     end
   end
 
   def star_track
-    TracksUser.create!(
+    @tracksUser = TracksUser.create!(
       event: Event.find(params["event"].to_i),
       track: Track.find(params["track"].to_i),
       source: "events",
       user: current_user)
-    redirect_to event_path(params["event"])
+      @event = @tracksUser.event
+      @track = @tracksUser.track
+      @number_of_likes = getLikes(@tracksUser)
+      render "events/refresh_card"
   end
 
   def unstar_track
-    TracksUser.find_by(
+    @tracksUser = TracksUser.find_by(
       event: Event.find(params["event"].to_i),
       track: Track.find(params["track"].to_i),
       source: "events",
-      user: current_user).destroy
-    redirect_to event_path(params["event"])
+      user: current_user)
+      @event = @tracksUser.event
+      @track = @tracksUser.track
+      @number_of_likes = getLikes(@tracksUser)
+      @tracksUser.destroy
+      render "events/refresh_card"
+  end
+
+  def getLikes(tracksUser)
+    @users = @event.users.where.not(id: current_user.id)
+    @track_counter = {}
+
+    @users.each do |user|
+      user.tracks_users.each do |user_track|
+        @track_counter[user_track.track_id] ||= [0, user_track.track]
+        @track_counter[user_track.track_id][0] += 1
+      end
+      @tracks = @track_counter.sort_by { |track_id, data| data.first }.take(20).map { |_, data| data.second }
+    end
+    @track_counter[@track.id][0]
   end
 
   def event_params
