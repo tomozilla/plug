364e6745d6951d6b6a11ef91d9fb3d80b64e2c72
Tomohiro <tmitani@heroku.com>
2020-03-23 19:25:35 +0900

    masking tokens

--- app/jobs/add_permission.rb
+++ app/jobs/add_permission.rb
@@ -3,8 +3,7 @@ class AddPermission < ApplicationJob
 
   def perform()
     puts "running"
-    API_TOKEN = '9a99385b-2f25-4d5f-91b5-5abfc1c3a79a'
-    heroku = PlatformAPI.connect_oauth(API_TOKEN)
+    heroku = PlatformAPI.connect_oauth(ENV['API_TOKEN'])
     puts heroku.app.info(ENV['HEROKU_APP_NAME'])
     puts "end"
   end
